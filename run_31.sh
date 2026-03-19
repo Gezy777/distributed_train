ad1c26db18fea28e94dc74b14db7d8b0277caf6b
Gezy777 <1768791459@qq.com>
2026-03-19 10:40:10 +0800

    synchronization

--- run_31.sh
+++ run_31.sh
@@ -1,6 +1,11 @@
+export NCCL_SOCKET_IFNAME=ens5f0np0
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
+export NCCL_DEBUG=INFO
+
 deepspeed --hostfile=hostfile \
           --master_addr=192.168.186.31 \
           --master_port=29500 \
-          --num_gpus=1 \
+          --num_gpus=2 \
           train.py \
           --deepspeed_config ds_config.json
