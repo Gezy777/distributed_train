424b35560f8c952b7c2a76aed2b93cbd2a63a861
Gezy777 <1768791459@qq.com>
2026-03-19 10:11:52 +0800

    synchronization

---
+++ run_31.sh
@@ -0,0 +1,6 @@
+deepspeed --hostfile=hostfile \
+          --master_addr=192.168.186.31 \
+          --master_port=29500 \
+          --num_gpus=2 \
+          train.py \
+          --deepspeed_config ds_config.json
